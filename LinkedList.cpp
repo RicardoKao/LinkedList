39e2dc3051e9b3ea2650d3bef30dad87dbccfa05
ricardogao <ricardogao@tencent.com>
2018-08-11 22:04:02 +0800

    Initial Commit

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -20,3 +20,23 @@ void LinkedList::insertNode( int position, int val){
         }
     }
 }
+
+void LinkedList::deleteNode(int position) {
+    ListNode *p = head;
+    if(head == nullptr){
+        cout <<"The LinkedList is empty. Delete failed."<<endl;
+    }else{
+        int pos = 0;
+        while(p!= nullptr && pos!= position-1){
+            p = p->next;
+            pos++;
+        }
+        if(p == nullptr)
+            cout <<"Position does not exist."<<endl;
+        else {
+            if(p->next==tail)
+                tail = p;
+            p->next = p->next->next;
+        }
+    }
+}
